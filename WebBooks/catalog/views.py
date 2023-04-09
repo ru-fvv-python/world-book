b0fef2c087db2f7a1a6063c2f39747eff3fbf897
vlad-fvv <vladfomin2008@gmail.com>
2023-04-09 12:09:01 +0300

    добавлена главная страница сайта

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,7 +1,27 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Book, Author, BookInstance
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Главная страница сайта Мир книг!')
+    """Генерация количеств некоторых объектов"""
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Доступные книги (статус "на складе")
+    num_instances_available = BookInstance.objects.filter(
+        status__exact=2).count()
+
+    # Авторы книг
+    num_authors = Author.objects.all().count()
+
+    # Отрисовка HTML шаблона index.html с данными
+    # внутри переменной context
+    return render(request, 'index.html',
+                  context={'num_books': num_books,
+                           'num_instances': num_instances,
+                           'num_instances_available': num_instances_available,
+                           'num_authors': num_authors},
+                  )
