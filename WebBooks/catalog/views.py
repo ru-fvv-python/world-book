c395d9ab4f6a5f855375cdcfe14945e187e77159
vlad-fvv <vladfomin2008@gmail.com>
2023-05-05 12:33:18 +0300

    добавлен счетчик посещний для главной страницы

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -34,11 +34,16 @@ def index(request):
     # Авторы книг
     num_authors = Author.objects.all().count()
 
+    # количество посещений этого view, посчитанное в переменной session
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Отрисовка HTML шаблона index.html с данными
     # внутри переменной context
     return render(request, 'index.html',
                   context={'num_books': num_books,
                            'num_instances': num_instances,
                            'num_instances_available': num_instances_available,
-                           'num_authors': num_authors},
+                           'num_authors': num_authors,
+                           'num_visits': num_visits},
                   )
