0be57e2f0497beae8e3b2c0cd6ad3558a1c8aa8f
vlad-fvv <vladfomin2008@gmail.com>
2023-05-13 12:03:09 +0300

    добавлен сброс пароля: - форма для ввода email и отправки ссылки на него - страничка с уведомлением об отправке ссылки на почту -

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -124,3 +124,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
